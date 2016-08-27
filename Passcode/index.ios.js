083a6882a31edb99e22f837faaa3b04f47379fdc
Ricky Padilla <rickycpadilla@gmail.com>
2016-08-27 17:54:18 -0600

    touch id and passcode working on iphone

--- Passcode/index.ios.js
+++ Passcode/index.ios.js
@@ -5,31 +5,63 @@
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, AlertIOS, TouchableHighlight, View, Text, StyleSheet } from 'react-native';
+ import PasscodeAuth from 'react-native-passcode-auth';
 
-class Passcode extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+ class Passcode extends Component {
+   _pressHandler() {
+     PasscodeAuth.authenticate('to demo this react-native component')
+       .then(success => {
+         AlertIOS.alert('Authenticated Successfully');
+         
+       })
+       .catch(error => {
+         AlertIOS.alert('Authentication Failed');
+       });
+   }
+
+   render() {
+     return (
+       <View style={styles.container}>
+         <TouchableHighlight onPress={this._pressHandler}>
+           <Text>
+             Authenticate with Passcode
+           </Text>
+         </TouchableHighlight>
+       </View>
+     );
+   }
+ };
+
+
+
+
+// import React, { Component } from 'react';
+// import {
+//   AppRegistry,
+//   StyleSheet,
+//   Text,
+//   View
+// } from 'react-native';
+//
+// class Passcode extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>
+//           Welcome to React Native!
+//         </Text>
+//         <Text style={styles.instructions}>
+//           To get started, edit index.ios.js
+//         </Text>
+//         <Text style={styles.instructions}>
+//           Press Cmd+R to reload,{'\n'}
+//           Cmd+D or shake for dev menu
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
 
 const styles = StyleSheet.create({
   container: {
